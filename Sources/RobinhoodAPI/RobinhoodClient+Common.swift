2612aef2d1808ec5e5afd04e8166abeb5c5df6b7
George Lo <twgeolo@gmail.com>
2021-05-07 02:06:01 -0700

    Allow fetching all results from paginated response

--- Sources/RobinhoodAPI/RobinhoodClient+Common.swift
+++ Sources/RobinhoodAPI/RobinhoodClient+Common.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 public extension RobinhoodClient {
 
@@ -13,6 +14,55 @@ public extension RobinhoodClient {
         @SafeOptionalValue public var next: URL?
         @SafeOptionalValue public var previous: URL?
         public let results: [T]
+    }
+    
+    func allResultsPublisher<T: Codable>(for response: PaginatedResponse<T>) -> AnyPublisher<[T], Error> {
+        func publisher(url: URL) -> AnyPublisher<PaginatedResponse<T>, Error> {
+            return getRequestPublisher(
+                token: lastAuthSuccessResponse!.accessToken, // FIXME: Auth
+                url: url
+            )
+            .map { $0.data }
+            .decode(type: PaginatedResponse<T>.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+        }
+        func recurse(url: URL) -> AnyPublisher<[T], Error> {
+            return publisher(url: url)
+                .flatMap { response -> AnyPublisher<[T], Error> in
+                    if let next = response.next {
+                        return recurse(url: next)
+                            .map { nextResponse in
+                                return response.results + nextResponse
+                            }
+                            .eraseToAnyPublisher()
+                    } else {
+                        return Just(response.results)
+                            .setFailureType(to: Error.self)
+                            .eraseToAnyPublisher()
+                    }
+                }
+                .eraseToAnyPublisher()
+        }
+        if let next = response.next {
+            return recurse(url: next)
+        } else {
+            return Just(response.results)
+                .setFailureType(to: Error.self)
+                .eraseToAnyPublisher()
+        }
+    }
+    
+    func allResultsPublisher<T: Codable>(for publisher: AnyPublisher<PaginatedResponse<T>, Error>) -> AnyPublisher<[T], Error> {
+        return publisher
+            .flatMap { [weak self] response -> AnyPublisher<[T], Error> in
+                guard let strongSelf = self else {
+                    return Just(response.results)
+                        .setFailureType(to: Error.self)
+                        .eraseToAnyPublisher()
+                }
+                return strongSelf.allResultsPublisher(for: response)
+            }
+            .eraseToAnyPublisher()
     }
 
 }
